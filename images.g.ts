57042a1f34df8c82e996bd4120c21d33a15468ae
Waylon Walker <quadmx08@gmail.com>
2022-08-26 08:41:11 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . c c c c . . . . . . . . . . 
+. . c c c c . . . . . . . . . . 
+. . c c c c . . . . . . . . . . 
+. . c c c c . . . . . . . . . . 
+. c c c c c c . . . . . . . . . 
+c . . 8 . . . c . . . . . . . . 
+. c 8 . 8 8 c . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
